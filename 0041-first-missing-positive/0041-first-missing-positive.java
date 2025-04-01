d467c906e8a16c30bd5da832b961e636e0f4b3e1
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-03-31 21:39:49 -0400

    Time: 14 ms (26.36%), Space: 61 MB (5.03%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,19 +1,18 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
 
         HashSet<Integer> set = new HashSet<>();
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<nums.length; i++){
             if(nums[i]>0){
                 set.add(nums[i]);
             }
         }
         int i=1;
         
-        while(i<Integer.MAX_VALUE){
-            // System.out.println(set.contains(i));
-
+        while(i<Integer.MAX_VALUE)
+        {
             if(!set.contains(i)) return i;
             i++;
         }
