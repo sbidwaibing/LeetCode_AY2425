4b110a2be24728fe5671123ada389e4836c9f4aa
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-04-23 23:30:56 -0400

    Time: 0 ms (100%), Space: 41.7 MB (87.08%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = n & 1;          // Extract the least significant bit
+            result = (result << 1) | bit; // Append the bit to the result
+            n = n >>> 1;             // Unsigned right-shift
+        }
+        return result;
+    }
+}
