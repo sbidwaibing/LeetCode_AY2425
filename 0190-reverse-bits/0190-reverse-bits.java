fcecccc2f1137f6004c4f67a49d2aa78b32aafa3
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-06-20 09:38:14 +0530

    Time: 0 ms (100%), Space: 41.7 MB (86.42%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,12 +1,12 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int result = 0;
-        for (int i = 0; i < 32; i++) {
-            int bit = n & 1;          // Extract the least significant bit
-            result = (result << 1) | bit; // Append the bit to the result
-            n = n >>> 1;             // Unsigned right-shift
+        int ans = 0;
+        for(int i =0; i<32; i++){
+            int bit = n & 1;
+            ans = (ans << 1) | bit;
+            n = n >>> 1;
         }
-        return result;
+        return ans;
     }
 }
