355c47554df798ceff21351eb41af050f9bdab3f
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-06-30 19:47:03 +0530

    Time: 0 ms (100%), Space: 50.1 MB (90.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,11 +1,24 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        int result = 0;
+        
+        // This solution utilizes a bruteforce technique with a TC - O(n);
+        // int n = nums.length;
+        // int result = 0;
 
-        for(int num : nums){
-            result ^= num;
+        // for(int num : nums){
+        //     result ^= num;
+        // }
+        // return result;
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(mid % 2 == 1) mid--;
+            if(nums[mid] == nums[mid + 1]) low = mid + 2;
+            else high = mid;
         }
-        return result;
+        return nums[high];
     }
 }
