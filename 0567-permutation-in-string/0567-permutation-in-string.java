148e8ce3cb3f3e13bd8464ea23dff9167cd3b203
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-07-07 19:45:31 +0530

    Time: 6 ms (87.36%), Space: 42.7 MB (79.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,29 +1,51 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
+        // if (s1.length() > s2.length()) return false;
             
+        // int[] count1 = new int[26];
+        // int[] count2 = new int[26];
+        
+        // for (char c : s1.toCharArray()) {
+        //     count1[c - 'a']++;
+        // }
+
+        // for(int i=0; i<s1.length(); i++){
+        //     count2[s2.charAt(i) - 'a']++;
+        // }
+
+        // if (matches(count1, count2)) return true;
+        
+        // for (int i = s1.length(); i < s2.length(); i++) {
+        // count2[s2.charAt(i) - 'a']++;              
+        // count2[s2.charAt(i - s1.length()) - 'a']--;
+        
+        //     if (matches(count1, count2)) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        if (s1.length() > s2.length()) return false;
+
         int[] count1 = new int[26];
         int[] count2 = new int[26];
-        
+
         for (char c : s1.toCharArray()) {
             count1[c - 'a']++;
         }
 
-        for(int i=0; i<s1.length(); i++){
+        for (int i = 0; i < s2.length(); i++) {
+            
             count2[s2.charAt(i) - 'a']++;
+
+            if (i >= s1.length()) {
+                count2[s2.charAt(i - s1.length()) - 'a']--;
+            }
+
+            if (matches(count1, count2)) return true;
         }
 
-        if (matches(count1, count2)) return true;
-        
-        for (int i = s1.length(); i < s2.length(); i++) {
-        count2[s2.charAt(i) - 'a']++;              
-        count2[s2.charAt(i - s1.length()) - 'a']--;
-        
-            if (matches(count1, count2)) {
-                return true;
-            }
-        }
-    return false;
+        return false;
     }
 
     private boolean matches(int[] count1, int[] count2){
