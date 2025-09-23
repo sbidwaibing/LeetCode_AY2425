8cbd755fa44f8f361723b6fce288cc02bb8d67c8
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-09-22 23:20:40 -0400

    Time: 28 ms (51.44%), Space: 66.8 MB (9.85%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
@@ -1,56 +1,31 @@
-// import java.math.BigInteger;
-// class Solution {
-//     public List<Integer> replaceNonCoprimes(int[] nums) {
-
-//         List<Integer> st = new ArrayList<>();
-
-//         for(int num : nums){
-
-//             int curr = num;
-
-//             while(st.size() > 0){
-//                 // gcd = findGCD(st.get(st.size() - 1), curr);
-//                 int gcd = BigInteger.valueOf(st.get(st.size() - 1)).gcd(BigInteger.valueOf(curr)).intValue();
-                
-//                 if(gcd == 1) break;
-//                 else{
-//                     long lcm = (1L * st.get(st.size() - 1) * curr) / gcd;
-//                     st.remove(st.size() - 1);
-//                     curr = (int)lcm;
-//                 }
-//             }
-//             st.add(curr);
-//         }
-//         return st;
-//     }
-
-//     // private int findGCD(int a, int b){
-//     //     if(a == 0) return b;
-//     //     return findGCD(b%a, a);
-//     // }
-// }
-
-import java.util.*;
-import java.math.BigInteger;
-
 class Solution {
     public List<Integer> replaceNonCoprimes(int[] nums) {
+
         List<Integer> st = new ArrayList<>();
+        int n = nums.length;
 
-        for (int num : nums) {
-            long curr = num;
+        for(int i=0; i<n; i++){
+            
+            int gcd = 0;
+            int curr = nums[i];
 
-            while (!st.isEmpty()) {
-                int top = st.get(st.size() - 1); // last element in stack
-                int gcd = BigInteger.valueOf(top).gcd(BigInteger.valueOf(curr)).intValue();
-
-                if (gcd == 1) break;
-                long lcm = (1L * top * curr) / gcd;
-                st.remove(st.size() - 1);
-                curr = lcm; // keep as long to avoid overflow
+            while(st.size() > 0){
+                gcd = findGCD(st.get(st.size() - 1), curr);
+                
+                if(gcd == 1) break;
+                else{
+                    long lcm = (1L * st.get(st.size() - 1) * curr) / gcd;
+                    st.remove(st.size() - 1);
+                    curr = (int)lcm;
+                }
             }
-            st.add((int)curr); // safe cast back to int at the end
+            st.add(curr);
         }
         return st;
+    }
+
+    private int findGCD(int a, int b){
+        if(a==0) return b;
+        return findGCD(b%a, a);
     }
 }
