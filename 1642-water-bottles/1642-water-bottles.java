dd0a047a2b90b51f79f3143116171abaea77c867
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-10-06 20:05:39 -0400

    Time: 0 ms (100%), Space: 40.7 MB (18.73%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.java
+++ 1642-water-bottles/1642-water-bottles.java
@@ -1,6 +1,15 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-         
-        return numBottles + (numBottles - 1) / (numExchange - 1);   
+        int ans=numBottles;
+        int remainingBottle=0;
+        while(numBottles>=numExchange){
+            remainingBottle = (numBottles%numExchange);
+            numBottles = (numBottles/numExchange);
+            ans+=numBottles;
+            numBottles+=remainingBottle;
+            // System.out.println(numBottles);
+        }
+        
+        return ans;
     }
 }
