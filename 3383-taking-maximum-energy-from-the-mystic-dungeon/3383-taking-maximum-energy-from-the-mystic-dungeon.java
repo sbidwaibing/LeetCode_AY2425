56421f0e72c39b63ced7be5617ace9da1f92b73d
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-10-10 19:43:38 -0400

    Time: 3 ms (55.28%), Space: 61.5 MB (49.07%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maximumEnergy(int[] energy, int k) {
+        
         int n = energy.length;
-        int[] matrix=new int[n];
-
+        int[] newEnergy = new int[n];
+        int ans = Integer.MIN_VALUE; 
+        
         for (int i = n - 1; i >= 0; i--) {
-            matrix[i] = energy[i];
+            newEnergy[i] = energy[i];
+            
             if (i + k < n) {
-                matrix[i] += matrix[i + k];
+                newEnergy[i] += newEnergy[i + k];
             }
-        }
-
-        int ans=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++)
-        {
-            ans=Math.max(matrix[i],ans);
+            
+            ans = Math.max(newEnergy[i],ans);
         }
 
         return ans;
