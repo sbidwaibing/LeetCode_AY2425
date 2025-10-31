ce65d255619a40c9f9fb2b91f2dce3bd7ec28732
Sukrut Bidwai <sbidwai@binghamton.edu>
2025-10-31 18:56:26 -0400

    Time: 0 ms (100%), Space: 44.3 MB (8.53%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -14,35 +14,13 @@ class Solution {
         if(list1 == null) return list2;
         if(list2 == null) return list1;
 
-        ListNode head;
-        
-        if(list1.val <= list2.val){
-            head = list1;
-            list1 = list1.next;
+        if(list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         }
         else{
-            head = list2;
-            list2 = list2.next;
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
         }
-
-        ListNode curr = head;
-
-        while(list1 != null && list2 != null){
-            if(list1.val <= list2.val){
-                curr.next = list1;
-                list1 = list1.next;
-            }
-            else{
-                curr.next = list2;
-                list2 = list2.next;
-            }
-
-            curr = curr.next;
-        }
-
-        if(list1 != null) curr.next = list1;
-        else curr.next = list2;
-        
-        return head;
     }
 }
